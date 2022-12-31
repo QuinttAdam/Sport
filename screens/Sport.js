889ef7c49225c96a9f192b25816304f59bd4e210
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2022-12-31 15:37:04 +0100

    style + shoppingcart

--- screens/Sport.js
+++ screens/Sport.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import {Stylesheet, Text, View, Image, TextInput, Pressable, FlatList, StyleSheet} from 'react-native';
+import {Text, View, Image, TextInput, Pressable, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
+
+
 
 
 const Sport = ({navigation}) =>{
@@ -23,29 +25,42 @@ const Sport = ({navigation}) =>{
         getSport();
     }, []);
 
+    const [pressCounter, setPressCounter]= useState(0);
+
+    function pressHandler() {
+    setPressCounter((currentPressCounter) =>  currentPressCounter+1);
+
+  }
     return (
-        <View >
+        <View style={styles.bg}>
+            <View >
+                <View style={styles.mand}><Image style={styles.pic} source={require("../assets/6011.png")}></Image></View>
+                <Text style={styles.aantal}>{pressCounter}</Text>
+            </View>
             <FlatList data ={Sport} renderItem={({item}) => (
                 
                 <View style={styles.container}>
                     
                 <View style={styles.sporttitle}>
-                    <View>
+                    <View >
                         {item.yoast_head_json?.og_image !== undefined && <Image
-                        style={{ width: 300, height: 200 }}
+                        style={{ width: 300, height: 200, borderRadius:5 }}
                         source={{ uri: `${item.yoast_head_json.og_image[0].url}` }}
                         />
                         }
                     </View>
                     <Text style={styles.title}>{item.title.rendered}</Text>
-                    {/* <Text>{item.rttpg_excerpt}</Text> */}
+                    
                     
 
                     
-                </View>
-                    <Pressable style={styles.btn} onPress={()=>navigation.navigate("Details", {itemTitle: item.title.rendered, itemDescription:item.rttpg_excerpt})}>
-                        <Text>Details</Text>
-                    </Pressable>
+                </View >
+                    <View style={styles.btns}>
+                        <Pressable style={styles.btn} onPress={()=>navigation.navigate("Details", {itemTitle: item.title.rendered, itemDescription:item.rttpg_excerpt})}>
+                            <Text>Details</Text>
+                        </Pressable>
+                        <TouchableOpacity activeOpacity={0.4} onPress={pressHandler}><Text style={styles.btn}>Koop ticket</Text></TouchableOpacity>
+                    </View>
                 </View>
             )}>
             </FlatList>
@@ -55,33 +70,59 @@ const Sport = ({navigation}) =>{
 
 }
 const styles = StyleSheet.create({
+    bg:{
+        backgroundColor: '#A9DAFF',
+    },
+    mand:{
+        flexDirection:"row",
+        justifyContent:"flex-end",
+        paddingRight:40,
+        paddingTop:15,
+        
+    },
+    pic:{
+        width:40,
+        height:40,
+        
+    },
+    aantal:{
+        position:'absolute',
+        right: 30,
+        top:50,
+        fontSize: 18,
+    },
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
+        backgroundColor: '#E2F3FF',
         margin: 30,
         marginBottom:-15,
         paddingTop: 30,
-        
+        borderRadius: 8,
     },
     title:{
-        backgroundColor:"#fff",
         fontSize:18,
         margin:10,
         
     },
     sporttitle:{
-        flexDirections: "column",
-        flexWrap: "wrap",
         alignItems: 'center',
     },
+    
     btn:{
         borderWidth: 1,
         borderBottomColor: "black",
-        borderRadius: 10,
+        borderRadius: 5,
         padding:5,
-        marginBottom: 20
+        marginBottom: 20,
+        
     },
+    
+    btns:{
+        marginTop: 10,
+        flex:1,
+        flexDirection:"row",
+        justifyContent:'space-evenly',
+        
+    }
   });
 
 export default Sport;
