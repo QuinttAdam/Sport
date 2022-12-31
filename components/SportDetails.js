da4337bf5b99540d650657a8a4b9c7fef3eb42e3
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2022-12-31 17:45:06 +0100

    style detailscreen

--- components/SportDetails.js
+++ components/SportDetails.js
@@ -1,21 +1,36 @@
 import React, {useState, useEffect} from 'react';
-import {Stylesheet, Text, View, Image, TextInput, Pressable, FlatList} from 'react-native';
+import {StyleSheet, Text, View, Image, TextInput, Pressable, FlatList} from 'react-native';
 
 
 const Detail = props =>{
 
 
     return (
-        <View>
-            <Text>
+        <View style={styles.bg}>
+            <Image 
+                style={{width:300, height:200, borderRadius:5}}
+                source={{uri: `${props.image}`}}/>
+            <Text style={styles.title}>
                 {props.title}
             </Text>
            <Text>
                 {props.description}
             </Text>
+            
         </View>
     );
 
 }
-
+const styles = StyleSheet.create({
+    bg:{
+        backgroundColor:"#E2F3FF",
+        padding:50,
+        paddingBottom:500,
+    },
+    title:{
+        fontSize:18,
+        marginTop:20,
+        marginBottom:5,
+    },
+})
 export default Detail;
