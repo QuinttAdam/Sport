66737b6f692214e8720abfc50a4467ef970bd34a
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-01-01 21:26:30 +0100

    updated main screen file met zoekbalk

--- screens/Sport.js
+++ screens/Sport.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Text, View, Image, TextInput, Pressable, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
-
+import Zoekbalk from '../components/zoekbalk';
 
 
 
@@ -25,112 +25,46 @@ const Sport = ({navigation}) =>{
         getSport();
     }, []);
 
-    const [pressCounter, setPressCounter]= useState(0);
+  const [input, setInput]= useState("");
+  console.log(input);
 
-    function pressHandler() {
-    setPressCounter((currentPressCounter) =>  currentPressCounter+1);
-
-  }
     return (
         <View style={styles.bg}>
             <View >
-                <View style={styles.mand}><Image style={styles.pic} source={require("../assets/6011.png")}></Image></View>
-                <Text style={styles.aantal}>{pressCounter}</Text>
-            </View>
-            <FlatList style={styles.posts} data ={Sport} renderItem={({item}) => (
-                
-                <View style={styles.container}>
-                    
-                <View style={styles.sporttitle}>
-                    <View >
-                        {item.yoast_head_json?.og_image !== undefined && <Image
-                        style={{ width: 300, height: 200, borderRadius:5 }}
-                        source={{ uri: `${item.yoast_head_json.og_image[0].url}` }}
-                        />
-                        }
-                    </View>
-                    <Text style={styles.title}>{item.title.rendered}</Text>
-                    
-                    
+                <TextInput
+                    value={input}
+                    placeholder="search sport"
+                    onChangeText={(text)=> setInput(text)}
+                    style={styles.inputveld}
+                />
 
-                    
-                </View >
-                    <View style={styles.btns}>
-                        <Pressable style={styles.btn} onPress={()=>navigation.navigate("Details", {itemTitle: item.title.rendered, itemDescription:item.rttpg_excerpt, itemImage: item.yoast_head_json.og_image[0].url})}>
-                            <Text>Details</Text>
-                        </Pressable>
-                        <TouchableOpacity activeOpacity={0.4} onPress={pressHandler}><Text style={styles.btn}>Koop ticket</Text></TouchableOpacity>
-                    </View>
-                </View>
-            )}>
-            </FlatList>
+            </View>
+            <Zoekbalk 
+                data={Sport}
+                input={input}
+                setInput={setInput}
+                navigation={navigation}
+            />
+            
         </View>
     );
 
 
 }
 const styles = StyleSheet.create({
-    posts:{
-        marginBottom:75,
-        
-    },
-
     bg:{
         backgroundColor: '#A9DAFF',
-        
+        paddingBottom:150,
     },
-    mand:{
-        flexDirection:"row",
-        justifyContent:"flex-end",
-        paddingRight:40,
-        paddingTop:15,
-        marginBottom:20,
-        
-    },
-    pic:{
-        width:40,
-        height:40,
-        
-    },
-    aantal:{
+    inputveld:{
         position:'absolute',
-        right: 30,
-        top:50,
-        fontSize: 18,
-    },
-    container: {
-        backgroundColor: '#E2F3FF',
-        marginTop: 10,
-        marginLeft:30,
-        marginRight:30,
-        marginBottom:20,
-        paddingTop:30,
-        borderRadius: 8,
-    },
-    title:{
+        left:30,
+        top:15,
         fontSize:18,
-        margin:10,
-        
-    },
-    sporttitle:{
-        alignItems: 'center',
-    },
-    
-    btn:{
-        borderWidth: 1,
-        borderBottomColor: "black",
-        borderRadius: 5,
-        padding:5,
-        marginBottom: 20,
-        
-    },
-    
-    btns:{
-        marginTop: 10,
-        flex:1,
-        flexDirection:"row",
-        justifyContent:'space-evenly',
-        
+        borderWidth:1,
+        padding:10,
+        paddingRight:150,
+        backgroundColor:"#E2F3FF"
     }
   });
 
