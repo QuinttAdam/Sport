2941cc22e6467f2abe874d9b7f5a93469db7fb25
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-01-06 01:32:15 +0100

    styling favoriete sporten lijst

--- screens/FavSports.js
+++ screens/FavSports.js
@@ -31,23 +31,31 @@ const FavSports=({navigation})=>{
     
 
     return(
-        <View>
+        <View  style={styles.container}>
+            <Text style={styles.h1}>Maak jouw eigen lijst van favoriete sporten</Text>
             <TextInput 
-            placeholder="Add sport"
+             style={styles.inputs}
+             placeholder="Add sport"
              onChangeText={SportNameHandler}/> 
+             
              <TextInput 
-            placeholder="Add description"
-             onChangeText={SportDescriptionHandler}/> 
+             style={styles.inputs2}
+             placeholder="Add description"
+             onChangeText={SportDescriptionHandler}
+             multiline
+             
+             /> 
 
 
-             <Pressable title="ADD" onPress={addSportHandler}><Text>Toevoegen</Text></Pressable>
+             <Pressable style={styles.btn} title="ADD" onPress={addSportHandler}><Text style={styles.toevoegen}>Toevoegen</Text></Pressable>
+             <Text style={styles.h2}>Dit is je lijst</Text>
              <FlatList 
-             style={{height:720}}
+             style={{height:460}}
                 data={Sports}
                 renderItem={({item}) => (
-                <View>
-                    <Text>{item.name}</Text>
-                    <Text>{item.description}</Text>
+                <View style={styles.list}>
+                    <Text style={styles.name}>{item.name}</Text>
+                    <Text style={styles.desc}>{item.description}</Text>
                 </View>
                  )}>
 
@@ -59,6 +67,82 @@ const FavSports=({navigation})=>{
 
 
 const styles = StyleSheet.create({
+    container:{
+       backgroundColor:"#A9DAFF",
+
+    },
+    h1:{
+        fontSize:18,
+        marginLeft:30,
+        marginTop:20,
+        fontWeight:'bold',
+    },
+    h2:{
+        fontSize:24,
+        marginLeft:30,
+        marginTop:20,
+        fontWeight:'bold',
+        alignItems:'center',
+        paddingBottom:10,
+    },
+    inputs:{
+        borderWidth:1,
+        padding:5,
+        marginLeft:30,
+        marginRight:30,
+        marginTop:10,
+        backgroundColor:"#E2F3FF",
+        fontSize:20,
+    },
+    inputs2:{
+        borderWidth:1,
+        padding:5,
+        paddingBottom:80,
+        marginLeft:30,
+        marginRight:30,
+        marginTop:10,
+        backgroundColor:"#E2F3FF",
+    },
+    btn:{
+        backgroundColor:"#E2F3FF",
+        borderWidth:1,
+        marginLeft:150,
+        marginRight:150,
+        marginBottom:10,
+        alignItems:'center',
+        paddingTop:10,
+        paddingBottom:10,
+        marginTop:20,
+        borderRadius:20,
+        
+    },
+    toevoegen:{
+        fontWeight:'bold',
+    },
+    list:{
+
+        backgroundColor:"#1c2d86",
+        marginLeft:30,
+        marginRight:30,
+        marginBottom:20,
+        marginTop:5,
+        borderRadius:5,
+        borderWidth:3,
+        borderColor:"#E2F3FF",
+        paddingLeft:20,
+        paddingRight:20,
+        paddingTop:10,
+        paddingBottom:10,
+    
+    },
+    name:{
+        color:"#E2F3FF",
+        fontSize:20,
+    },
+    desc:{
+        paddingTop:5,
+        color:"#E2F3FF",
+    },
 
 })
 
