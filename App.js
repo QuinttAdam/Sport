559b402e17b03f711621ff6d404cadb755877ea6
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-01-13 18:06:47 +0100

    Beginscherm toegevoegd

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import{NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import Beginscherm from './screens/Beginscherm';
 import Sport from './screens/Sport';
 import Details from './screens/Details';
 import FavSports from './screens/FavSports';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 
 
 const Stack =createNativeStackNavigator();
@@ -14,6 +16,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen name="Beginscherm" component={Beginscherm}/>
         <Stack.Screen name="Sport" component={Sport}/>  
         <Stack.Screen name="Details" component={Details}/>  
         <Stack.Screen name="Favourite Sports" component={FavSports}/>  
