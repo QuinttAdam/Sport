0fdec5ac0931a733af474223347b9738259145af
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-01-14 19:18:55 +0100

    eindproduct screen Details.js

--- screens/Details.js
+++ screens/Details.js
@@ -1,20 +1,16 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, Image, TextInput, Pressable, FlatList} from 'react-native';
-import Detail from '../components/SportDetails';
+import { View } from "react-native";
+import Detail from "../components/SportDetails";
 
-const Details = ({navigation, route}) =>{
-
-
-    return (
-        <View>
-            <Detail 
-            title={route.params.itemTitle}
-            description={route.params.itemDescription}
-            image={route.params.itemImage}
-            />
-        </View>
-    );
-
-}
+const Details = ({ route }) => {
+  return (
+    <View>
+      <Detail
+        title={route.params.itemTitle}
+        description={route.params.itemDescription}
+        image={route.params.itemImage}
+      />
+    </View>
+  );
+};
 
 export default Details;
