0f91964a74675d3b02862cc58f120abbf378814f
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-01-14 19:15:08 +0100

    eindproduct App.js

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
-import{NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Beginscherm from './screens/Beginscherm';
-import Sport from './screens/Sport';
-import Details from './screens/Details';
-import FavSports from './screens/FavSports';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Beginscherm from "./screens/Beginscherm";
+import Sport from "./screens/Sport";
+import Details from "./screens/Details";
+import FavSports from "./screens/FavSports";
 
-
-
-const Stack =createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name="Beginscherm" component={Beginscherm}/>
-        <Stack.Screen name="Sport" component={Sport}/>  
-        <Stack.Screen name="Details" component={Details}/>  
-        <Stack.Screen name="Favourite Sports" component={FavSports}/>  
+        <Stack.Screen name="Beginscherm" component={Beginscherm} />
+        <Stack.Screen name="Sport" component={Sport} />
+        <Stack.Screen name="Details" component={Details} />
+        <Stack.Screen name="Favourite Sports" component={FavSports} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
-
