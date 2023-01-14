328b1ebe750127fb1448c3558a758f68547c4f57
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-01-14 19:17:01 +0100

    eindproduct component SportDetails.js

--- components/SportDetails.js
+++ components/SportDetails.js
@@ -1,36 +1,27 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, Image, TextInput, Pressable, FlatList} from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
 
-
-const Detail = props =>{
-
-
-    return (
-        <View style={styles.bg}>
-            <Image 
-                style={{width:300, height:200, borderRadius:5}}
-                source={{uri: `${props.image}`}}/>
-            <Text style={styles.title}>
-                {props.title}
-            </Text>
-           <Text>
-                {props.description}
-            </Text>
-            
-        </View>
-    );
-
-}
+const Detail = (props) => {
+  return (
+    <View style={styles.bg}>
+      <Image
+        style={{ width: 300, height: 200, borderRadius: 5 }}
+        source={{ uri: `${props.image}` }}
+      />
+      <Text style={styles.title}>{props.title}</Text>
+      <Text>{props.description}</Text>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    bg:{
-        backgroundColor:"#E2F3FF",
-        padding:50,
-        paddingBottom:500,
-    },
-    title:{
-        fontSize:18,
-        marginTop:20,
-        marginBottom:5,
-    },
-})
+  bg: {
+    backgroundColor: "#E2F3FF",
+    padding: 50,
+    paddingBottom: 500,
+  },
+  title: {
+    fontSize: 18,
+    marginTop: 20,
+    marginBottom: 5,
+  },
+});
 export default Detail;
